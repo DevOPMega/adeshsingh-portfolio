e71a684df8661787a0b3901c02e572b47a7b968a
Adesh Singh <adeshsingh3002@gmail.com>
2024-07-12 21:57:53 +0530

    Add key props in map

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Skills() {
   const skills = [
-      { skillName: "Typescript", class:"bg-red-800 text-white" },
-      { skillName: "Tailwind CSS", class:"bg-blue-700 text-white" },
-    { skillName: "React JS", class:"bg-yellow-500 text-black"  },
-    { skillName: "Next JS", class:"bg-green-500 text-black" },
-    { skillName: "Node JS", class:"bg-cyan-500 text-black" },
-    { skillName: "MongoDB", class:"bg-purple-800 text-white" },
+    { skillName: "Typescript", class: "bg-red-800 text-white" },
+    { skillName: "Tailwind CSS", class: "bg-blue-700 text-white" },
+    { skillName: "React JS", class: "bg-yellow-500 text-black" },
+    { skillName: "Next JS", class: "bg-green-500 text-black" },
+    { skillName: "Node JS", class: "bg-cyan-500 text-black" },
+    { skillName: "MongoDB", class: "bg-purple-800 text-white" },
   ];
 
   return (
@@ -18,11 +18,14 @@ export default function Skills() {
           <Image src="/img/skills.png" alt="skills" width="64" height="64" />
         </h1>
         <div className="py-6 flex justify-center flex-wrap gap-8">
-            {skills.map((skill, i) => (
-                <div className={`${skill.class} px-4 py-3 rounded-md font-bold text-xl`}>
-                    {skill.skillName}
-                </div>   
-            ))}
+          {skills.map((skill, i) => (
+            <div
+              key={i}
+              className={`${skill.class} px-4 py-3 rounded-md font-bold text-xl`}
+            >
+              {skill.skillName}
+            </div>
+          ))}
         </div>
       </div>
     </>
