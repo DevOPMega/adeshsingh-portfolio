40a7df96459638a0136a9ad4d9a17c4d58296188
Adesh Singh <adeshsingh3002@gmail.com>
2024-07-17 00:49:32 +0530

    Form for signup

--- app/components/SignupForm.tsx
+++ app/components/SignupForm.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { useForm } from "react-hook-form";
 import type { FieldValues } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { signupUser } from "@/utils/signin";
+import Toast from "./Toast";
+
+const signupURI = process.env.NEXT_PUBLIC_API_REGISTER || "";
 
 export default function SignupForm() {
   const {
@@ -10,70 +17,78 @@ export default function SignupForm() {
     reset,
     getValues,
   } = useForm();
+  const router = useRouter();
 
   const onSubmit = async (data: FieldValues) => {
-    const response = await fetch("http://localhost:5000/api/register", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include',
-      body: JSON.stringify(data)
-    });
-    console.log(response.json());
+    const response = await signupUser(data);
+    const parseResponse = await response.json();
+    if (response.status === 201) {
+      toast.success(`${parseResponse.message}`);
+      setTimeout(() => {
+        router.push("/login");
+      }, 5000);
+    } else if (response.status === 500) {
+      toast.error(`${parseResponse.error}`);
+    } else if (response.status === 422) {
+      toast.error(`${parseResponse.email} ${parseResponse.password}`);
+    }
     reset();
   };
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col mt-10 space-y-4"
-    >
-      <input
-        {...register("email", {
-          required: "Email is required",
-        })}
-        type="email"
-        className="w-full p-2 text-lg text-black outline-none rounded-md focus:outline-4 focus:outline-blue-500 placeholder:text-slate-500"
-        placeholder="john12@gmail.com"
-      />
-      {errors.email && (
-        <p className="text-base text-yellow-400">{`${errors.email.message}`}</p>
-      )}
-      <input
-        {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 10,
-            message: "Password must be at least 10 characters",
-          },
-        })}
-        type="password"
-        className="w-full p-2 text-lg text-black outline-none rounded-md focus:outline-4 focus:outline-blue-500  placeholder:text-slate-500"
-        placeholder="password"
-      />
-      {errors.password && (
-        <p className="text-base text-yellow-400">{`${errors.password.message}`}</p>
-      )}
-      <input
-        {...register("confirmPassword", {
-          required: "Confirm Password is required",
-          validate: (value) =>
-            value === getValues("password") || "Password must match",
-        })}
-        type="password"
-        className="w-full p-2 text-lg text-black outline-none rounded-md focus:outline-4 focus:outline-blue-500 placeholder:text-slate-500"
-        placeholder="confirm password"
-      />
-      {errors.confirmPassword && (
-        <p className="text-base text-yellow-400">{`${errors.confirmPassword.message}`}</p>
-      )}
-      <button
-        type="submit"
-        disabled={isSubmitting}
-        className="p-2 font-bold text-lg rounded-md bg-blue-600"
+    <>
+      <Toast />
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col mt-10 space-y-4"
       >
-        Signup
-      </button>
-    </form>
+        <input
+          {...register("email", {
+            required: "Email is required",
+          })}
+          type="email"
+          className="w-full p-2 text-lg text-black outline-none rounded-md focus:outline-4 focus:outline-blue-500 placeholder:text-slate-500"
+          placeholder="john12@gmail.com"
+        />
+        {errors.email && (
+          <p className="text-base text-yellow-400">{`${errors.email.message}`}</p>
+        )}
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 10,
+              message: "Password must be at least 10 characters",
+            },
+          })}
+          type="password"
+          className="w-full p-2 text-lg text-black outline-none rounded-md focus:outline-4 focus:outline-blue-500  placeholder:text-slate-500"
+          placeholder="password"
+        />
+        {errors.password && (
+          <p className="text-base text-yellow-400">{`${errors.password.message}`}</p>
+        )}
+        <input
+          {...register("confirmPassword", {
+            required: "Confirm Password is required",
+            validate: (value) =>
+              value === getValues("password") || "Password must match",
+          })}
+          type="password"
+          className="w-full p-2 text-lg text-black outline-none rounded-md focus:outline-4 focus:outline-blue-500 placeholder:text-slate-500"
+          placeholder="confirm password"
+        />
+        {errors.confirmPassword && (
+          <p className="text-base text-yellow-400">{`${errors.confirmPassword.message}`}</p>
+        )}
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className="p-2 font-bold text-lg rounded-md bg-blue-600"
+        >
+          Signup
+        </button>
+      </form>
+    </>
   );
 }
