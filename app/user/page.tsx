addeb357c56cef3545fe54ef9d3015693c911d0a
Adesh Singh <adeshsingh3002@gmail.com>
2024-07-19 14:43:06 +0530

    Change layout of page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,9 +2,11 @@ import UserProfile from "../components/UserProfile";
 
 export default function page() {
   return (
-    <div>
-      <h1>User Page</h1>
-      <UserProfile />
+    <div className="w-5/6 my-16 mx-auto bg-slate-300/30 backdrop-blur-sm rounded-lg md:w-1/2 lg:w-2/5">
+      <div className="py-8 px-4">
+        <h1 className="text-center text-4xl font-bold">Profile</h1>
+        <UserProfile />
+      </div>
     </div>
   )
 }
